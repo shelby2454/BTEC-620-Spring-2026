baef741e9aa40b5bc0e6f4026b5c090bd15d067a
shelby2454 <143620008+shelby2454@users.noreply.github.com>
2026-03-26 13:47:21 -0400

    Add files via upload

---
+++ Phylogentic-Tree.R
@@ -0,0 +1,20 @@
+#Author:Shelby Thomas, Date: March 26, 2026, Purpose: To generate a phylogentic tree structure.
+
+#Load package ape
+library(ape)
+
+#Generate some dummy data for tree structure
+
+text.string<- "(((((((cow, pig),whale),(bat,(lemur,human))),(robin,iguana)),coelacanth),gold_fish),shark);"
+
+vert.tree<-read.tree(text=text.string)
+
+#Plot the tree structure
+
+plot(vert.tree,no.margin=TRUE,edge.width=2)
+
+#Writing/storing the tree structure dataset
+
+setwd("/Users/shelbythomas/Desktop")
+
+write.tree(vert.tree,file="dummy.tre")
